0441ac8eb6e9401da2b4b1f9cf0f12affef61804
shunzhy6 <2533058830@qq.com>
2021-03-25 20:35:59 +0800

    牛客网

---
+++ leetcode/src/niuke/NC101.java
@@ -0,0 +1,22 @@
+package niuke;
+
+/**
+ *从0,1,2,...,n这n+1个数中选择n个数，组成有序数组，请找出缺失的那个数，要求O(n)尽可能小。
+ * 示例1
+ * 输入
+ * [0,1,2,3,4,5,7]
+ * 返回值
+ * 6
+*/
+public class NC101 {
+
+    /**
+     * 找缺失数字
+     * @param a int整型一维数组 给定的数字串
+     * @return int整型
+     */
+    public int solve (int[] a) {
+        // write code here
+    }
+
+}
---
+++ leetcode/src/niuke/NC45.java
@@ -0,0 +1,31 @@
+package niuke;
+
+/**
+ *  分别按照二叉树先序，中序和后序打印所有的节点。
+ * 示例1
+ * 输入
+ * {1,2,3}
+ * 返回值
+ * [[1,2,3],[2,1,3],[2,3,1]]
+ * 备注:
+ * n≤10^6
+*/
+public class NC45 {
+
+    /*
+     * public class TreeNode {
+     *   int val = 0;
+     *   TreeNode left = null;
+     *   TreeNode right = null;
+     * }
+     */
+    /**
+     *
+     * @param root TreeNode类 the root of binary tree
+     * @return int整型二维数组
+     */
+    public int[][] threeOrders (TreeNode root) {
+        // write code here
+    }
+
+}
